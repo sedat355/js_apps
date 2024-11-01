7f2133e8f5dfdc9e875c304859f97bd5d9ea4281
Sedat Polat <sedatpolat355@gmail.com>
2024-11-01 10:18:34 +0300

    html and css ready

---
+++ clipboard/clipboard.js
@@ -0,0 +1,12 @@
+const container = document.querySelector(".container")
+const input = document.querySelector("[data-input]")
+const copyBtn = document.querySelector("[data-btn]")
+
+
+//!Eski yöntem:
+// copyBtn.addEventListener('click', copy)
+
+// function copy() {
+//   input.select()
+//   document.execCommand('copy') 
+// }
