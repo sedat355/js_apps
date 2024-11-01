0e7211700f2f180624d5701f8e2115b276760600
Sedat Polat <sedatpolat355@gmail.com>
2024-11-01 11:27:47 +0300

    dcode's clipboard project completed

--- clipboard/clipboard.js
+++ clipboard/clipboard.js
@@ -1,7 +1,40 @@
 const container = document.querySelector(".container")
 const input = document.querySelector("[data-input]")
-const copyBtn = document.querySelector("[data-btn]")
+const copyButton = document.querySelector("[data-copy-btn]")
+const copyButtons = document.querySelectorAll("[data-copy-btn]")
+// const copyButtons = document.querySelectorAll(".copy")
 
+console.log(copyButton)
+
+copyButton.addEventListener('click', () => {
+  const targetElement = document.querySelector(copyButton.dataset.copy)
+  //const textToCopy = targetElement.innerText//veya
+
+  const textToCopy = targetElement.textContent
+    .replace(/\s+/g, " ")
+    .trim();
+
+  navigator.clipboard.writeText(textToCopy).then(() => {
+    const label = copyButton.querySelector('.copy-label')
+    const originalText = label.textContent
+
+    copyButton.disabled = true
+    label.textContent = "Copied!"
+
+    setTimeout(() => {
+      copyButton.disabled = false
+      label.textContent = originalText
+    }, 1000);
+  })
+})
+
+//Aşağıdaki forEach çalışmadı nedense ben de tek buton üzerinden yaptım!!
+// copyButtons.forEach(copyBtn => () => {
+//   copyBtn.addEventListener('click', () => {
+//     const targetElement = copyBtn.dataset.copy
+//     console.log(targetElement)
+//   })
+// })
 
 //!Eski yöntem:
 // copyBtn.addEventListener('click', copy)
